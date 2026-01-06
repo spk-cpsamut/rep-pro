2dcea244a84171b802820fcf43a39bdb01fa7d41
Suprakon Cheepsamut <scheepsamut@sureco.com>
2026-01-06 19:30:50 +0700

    Update error message in main function to clarify failure context for client encryption

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use clilent::Client;
 async fn main() ->  Result<(), Box<dyn std::error::Error>>{
     dotenvy::dotenv().ok();
     let client = Client::new();
-    server::encrypt(client).await.expect("server encrypt failed");
+    server::encrypt(client).await.expect("resource from client encrypt failed");
 
     Ok(())
 }
