324e43d7fa8499214c750c619209168728ab6e6d
Suprakon Cheepsamut <scheepsamut@sureco.com>
2026-07-05 09:27:15 +0700

    cleanup project

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,6 @@
-mod clilent;
-mod server;
-use clilent::Client;
-use sqlx::postgres::PgPoolOptions;
-use std::env;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    dotenvy::dotenv().ok();
-    let client = Client::new();
-    let pool = PgPoolOptions::new()
-        .max_connections(10)
-        .connect(&env::var("DATABASE_URL")?)
-        .await?;
-    server::encrypt(client, &pool)
-        .await
-        .expect("resource from client encrypt failed");
 
     Ok(())
 }
